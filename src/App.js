1c4f81a1ef81da26da388a09b4cc87afe0ec1992
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-18 12:11:09 +0530

    Added the final css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { CardList} from './components/card-list/card-list.component'
+import { CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -21,17 +22,22 @@ class App extends Component {
     }))
   }
 
+  handleChange = (e) =>{
+    this.setState({searchField : e.target.value})
+  }
+
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
-      
+
     return(
       <div className="App">
-      <input
-      type = 'search'
-      placeholder='search monsters'
-      onChange = {e => this.setState({searchField : e.target.value}) }
+      <h1> Monsters Rolodex
+      </h1>
+      <SearchBox
+      placeholder ='search monsters'
+      handleChange = {this.handleChange}
       />
       <CardList  monsters = {filteredMonsters}>
       </CardList>
