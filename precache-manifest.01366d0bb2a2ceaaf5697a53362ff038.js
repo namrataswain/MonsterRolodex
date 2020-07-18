0e0be55519a910dcc8a8bb046d3db35a21a67186
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-18 12:28:04 +0530

    Updates

---
+++ precache-manifest.01366d0bb2a2ceaaf5697a53362ff038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbebff08dfea11c898969ff5c65ad936",
+    "url": "/monsterRolodex/index.html"
+  },
+  {
+    "revision": "61cbee5282e98f018a72",
+    "url": "/monsterRolodex/static/css/main.039b5c6d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsterRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cbee5282e98f018a72",
+    "url": "/monsterRolodex/static/js/main.f7e2ff3d.chunk.js"
+  },
+  {
+    "revision": "47b46d6f15d610d10ab6",
+    "url": "/monsterRolodex/static/js/runtime-main.83640a90.js"
+  }
+]);
